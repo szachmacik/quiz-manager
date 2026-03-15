7eb71c29eec9e80338e26f3fcd284292d61b73db
szachmacik <maciej.koziej01@gmail.com>
2026-03-16 00:29:37 +0100

    fix: resolve @shared alias in routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { COOKIE_NAME } from "@shared/const";
+import { COOKIE_NAME } from "../shared/const";
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, protectedProcedure, router } from "./_core/trpc";
